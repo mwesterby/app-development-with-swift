c8f1ee9354211b4bf06505e325002d677e69f932
Michael-Westerby <michael.westerby@ibm.com>
2020-04-24 11:22:31 +0100

    Completed Auto Layout and Stack Views

---
+++ UI Kit/Auto Layout and Stack Views/AutoLayoutPractice/AutoLayoutPractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AutoLayoutPractice
+//
+//  Created by Michael Westerby on 17/04/2020.
+//  Copyright © 2020 Michael Westerby. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
