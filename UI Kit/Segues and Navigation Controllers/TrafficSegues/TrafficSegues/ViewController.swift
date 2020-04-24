128235c6e5513178197d64728e7713e2317bcfdb
Michael-Westerby <michael.westerby@ibm.com>
2020-04-24 11:23:08 +0100

    Completed Segues and Navigation Controllers

---
+++ UI Kit/Segues and Navigation Controllers/TrafficSegues/TrafficSegues/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  TrafficSegues
+//
+//  Created by Michael Westerby on 22/04/2020.
+//  Copyright © 2020 Michael Westerby. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var segueSwitch: UISwitch!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBAction func yellowButtonTapped(_ sender: Any) {
+        if segueSwitch.isOn {
+            performSegue(withIdentifier: "Yellow", sender: nil)
+        }
+    }
+    
+    @IBAction func greenButtonTapped(_ sender: Any) {
+        if segueSwitch.isOn {
+            performSegue(withIdentifier: "Green", sender: nil)
+        }
+    }
+    
+}
+
