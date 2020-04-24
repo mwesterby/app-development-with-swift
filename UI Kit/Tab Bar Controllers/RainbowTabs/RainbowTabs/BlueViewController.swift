9388beaf553dad97e907bbac4444b9d95f75c734
Michael-Westerby <michael.westerby@ibm.com>
2020-04-24 11:23:32 +0100

    Completed Tab Bar Controllers

---
+++ UI Kit/Tab Bar Controllers/RainbowTabs/RainbowTabs/BlueViewController.swift
@@ -0,0 +1,30 @@
+//
+//  BlueViewController.swift
+//  RainbowTabs
+//
+//  Created by Michael Westerby on 24/04/2020.
+//  Copyright © 2020 Michael Westerby. All rights reserved.
+//
+
+import UIKit
+
+class BlueViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
