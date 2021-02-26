b2c55051b2c3ecdd5edc6693871bf7a33a54de97
Michael Westerby <48826192+mwesterby@users.noreply.github.com>
2021-02-26 16:18:37 +0000

    Finished Introduction to Swift and Playgrounds

---
+++ Unit 1/1.1 Introduction to Swift and Playgrounds/Lab - Introduction.playground/Contents.swift
@@ -0,0 +1,22 @@
+/*: 
+ ## Exercise: Use Playgrounds
+ 
+ The code below prints a few short statements about what you have learned in this lesson. Open the console area and view the code's output.
+ */
+
+print("I have learned the following:")
+print("What features make Swift a modern and safe language")
+print("How to use the Swift REPL in Terminal")
+print("How to use playgrounds to make writing Swift fun and simple")
+ 
+/*:
+ Now print your own phrases to the console. Pick one of your favorite songs. Use your knowledge of the `print` function to display the song title and artist.
+ */
+
+
+/*:
+ Use multiple `print` functions to write out some of the lyrics to the song.
+ */
+
+
+//:page 14 of 16  |  [Next: Exercise: Go! Fight! Win!](@next)
---
+++ Unit 1/1.1 Introduction to Swift and Playgrounds/Lab - Introduction.playground/Pages/1. Exercise - Use Playgrounds.xcplaygroundpage/Contents.swift
@@ -0,0 +1,31 @@
+/*:
+ ## Exercise - Use Playgrounds
+
+ The code below prints a few short statements about what you have learned in this lesson. Open the console area and view the code's output.
+ */
+print("I have learned the following:")
+print("What features make Swift a modern and safe language")
+print("How to use the Swift REPL in Terminal")
+print("How to use playgrounds to make writing Swift fun and simple")
+/*:
+ Now print your own phrases to the console. Pick one of your favorite songs. Use your knowledge of the `print` function to display the song title and artist.
+ */
+print("Rolling in the Deep")
+print("Adele")
+/*:
+ Use multiple `print` functions to write out some of the lyrics to the song.
+ */
+print("There's a fire starting in my heart")
+print("Reaching a fever pitch and it's bringing me out the dark")
+print("Finally, I can see you crystal clear")
+print("Go ahead and sell me out and I'll lay your ship bare")
+/*:
+
+ _Copyright © 2018 Apple Inc._
+
+ _Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:_
+
+ _The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software._
+
+ _THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE._
+ */
